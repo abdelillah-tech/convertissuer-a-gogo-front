03bce0f01f2940cb352dc335a81d377be3d1216c
anonymax25 <32836662+anonymax25@users.noreply.github.com>
2021-07-25 23:45:11 +0000

    Deploying to release from  @ e6bf5d39c58bec51854ad954e8f97e2fdd0df747 🚀

---
+++ precache-manifest.0faadadffb4472f5feb34dae4d948748.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ca2bbb907bf1c327d85d4105136085b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "85fb2c53ab3e9e82dd08",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "2074074b4dc824c96954",
+    "url": "/static/js/2.05d08e2f.chunk.js"
+  },
+  {
+    "revision": "89a1b2dcd30c03705b2bceeb141b76b6",
+    "url": "/static/js/2.05d08e2f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85fb2c53ab3e9e82dd08",
+    "url": "/static/js/main.4a5b4276.chunk.js"
+  },
+  {
+    "revision": "33e712277cd1a8d47f5d",
+    "url": "/static/js/runtime-main.0cc416c7.js"
+  }
+]);
