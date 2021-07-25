4cb5759cf92a15833f5a888e4b9b3011e763f9e5
maxime.dharboulle <maxime.dharboulle@sequodia.com>
2021-07-25 18:37:10 +0200

    Updates

---
+++ precache-manifest.b08abb27d8e86f3f05bb3a4eb8116a86.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe27a372c32d8993b2d1615b037c7db1",
+    "url": "/convertissuer-a-gogo-front/index.html"
+  },
+  {
+    "revision": "cb477752bc99b76f1c0d",
+    "url": "/convertissuer-a-gogo-front/static/css/main.7f904051.chunk.css"
+  },
+  {
+    "revision": "b220d851f4997d17cce8",
+    "url": "/convertissuer-a-gogo-front/static/js/2.8bbd6e48.chunk.js"
+  },
+  {
+    "revision": "89a1b2dcd30c03705b2bceeb141b76b6",
+    "url": "/convertissuer-a-gogo-front/static/js/2.8bbd6e48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb477752bc99b76f1c0d",
+    "url": "/convertissuer-a-gogo-front/static/js/main.d1c07f73.chunk.js"
+  },
+  {
+    "revision": "4645d94d65b48ee715bf",
+    "url": "/convertissuer-a-gogo-front/static/js/runtime-main.1d645230.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/convertissuer-a-gogo-front/precache-manifest.b08abb27d8e86f3f05bb3a4eb8116a86.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/convertissuer-a-gogo-front/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
