c8a8872e76cdba5b88197a332d3bd165ad5fce0b
anonymax25 <32836662+anonymax25@users.noreply.github.com>
2021-07-29 03:08:06 +0000

    Deploying to release from  @ 62019f02ec5dce8b83ffa90b254b6a9fb311f2c7 🚀

--- precache-manifest.e06df9ebcbda0c6aa60a75106e6f29d3.js
+++ precache-manifest.e06df9ebcbda0c6aa60a75106e6f29d3.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "2141efcf3f4d559bbf6dab882159ea52",
+    "revision": "4da58afeb8d334913ad8c883d865c7c8",
     "url": "/index.html"
   },
   {
-    "revision": "c68881dde1cca619e7c1",
+    "revision": "3c8a03a5a48e88cfef6c",
     "url": "/static/css/main.d1b05096.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.b74f41ff.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "c68881dde1cca619e7c1",
-    "url": "/static/js/main.96a4c8c3.chunk.js"
+    "revision": "3c8a03a5a48e88cfef6c",
+    "url": "/static/js/main.29417f8d.chunk.js"
   },
   {
     "revision": "33e712277cd1a8d47f5d",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6d93273d282375b2c2d6a94547d48d02.js"
+  "/precache-manifest.e06df9ebcbda0c6aa60a75106e6f29d3.js"
 );
 
 self.addEventListener('message', (event) => {
